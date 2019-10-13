2bf0bf731a6a8f830c752409fa80bb1c57dc8404
Scott Davis <scott@thirstyhead.com>
2019-10-13 11:19:52 -0400

    adding deleting/copying comments

--- __reset.sh
+++ __reset.sh
@@ -1,13 +1,25 @@
 # copies target website into current directory
 # NOTE: this is destructive!!! Be careful!
 if [[ $# -eq 0 ]] ; then
-  echo USAGE: $(basename "$0") dir-to-cp
+  echo USAGE: $(basename "$0") dir-to-cp-from
   exit 1
 fi
 
+echo $'\nDeleting:'
+echo '---------'
 rm -v index.html
+echo $'\nCopying:'
+echo '---------'
 cp -v $1/index.html .
+echo $'\nDeleting:'
+echo '---------'
 rm -rfv css
+echo $'\nCopying:'
+echo '---------'
 cp -rv $1/css .
+echo $'\nDeleting:'
+echo '---------'
 rm -rfv components
+echo $'\nCopying:'
+echo '---------'
 cp -rv $1/components .
--- __set.sh
+++ __set.sh
@@ -1,4 +1,25 @@
-# copies current website into a target directory
+# copies current website into target directory
+# NOTE: this is destructive!!! Be careful!
+if [[ $# -eq 0 ]] ; then
+  echo USAGE: $(basename "$0") dir-to-cp-to
+  exit 1
+fi
+
+echo $'\nDeleting:'
+echo '---------'
+rm -v $1/index.html
+echo $'\nCopying:'
+echo '---------'
 cp -v index.html $1
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv $1/css
+echo $'\nCopying:'
+echo '---------'
 cp -rv css $1
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv $1/components
+echo $'\nCopying:'
+echo '---------'
 cp -rv components $1
