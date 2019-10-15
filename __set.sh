1f9c1b65ddd61a7b7f2d4400eee8a699261de855
Scott Davis <scott@thirstyhead.com>
2019-10-15 14:16:46 -0400

    tweaked __set.sh to include schedule, speakers

--- __set.sh
+++ __set.sh
@@ -5,21 +5,45 @@ if [[ $# -eq 0 ]] ; then
   exit 1
 fi
 
+# index.html
 echo $'\nDeleting:'
 echo '---------'
 rm -v $1/index.html
 echo $'\nCopying:'
 echo '---------'
 cp -v index.html $1
+
+# css
 echo $'\nDeleting:'
 echo '---------'
 rm -rfv $1/css
 echo $'\nCopying:'
 echo '---------'
 cp -rv css $1
+
+# components
 echo $'\nDeleting:'
 echo '---------'
 rm -rfv $1/components
 echo $'\nCopying:'
 echo '---------'
 cp -rv components $1
+
+# speakers
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv $1/speakers
+echo $'\nCopying:'
+echo '---------'
+cp -rv speakers $1
+
+# schedule 
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv $1/schedule
+echo $'\nCopying:'
+echo '---------'
+cp -rv schedule $1
+
+
+
