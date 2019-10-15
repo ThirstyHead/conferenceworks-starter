6278edd80a8003980e6b0e7475ae0d1d41817e42
Scott Davis <scott@thirstyhead.com>
2019-10-15 14:28:00 -0400

    tweaked __reset.sh to include schedule, speakers

--- __reset.sh
+++ __reset.sh
@@ -5,21 +5,46 @@ if [[ $# -eq 0 ]] ; then
   exit 1
 fi
 
+# index.html
 echo $'\nDeleting:'
 echo '---------'
 rm -v index.html
 echo $'\nCopying:'
 echo '---------'
 cp -v $1/index.html .
+
+# css
 echo $'\nDeleting:'
 echo '---------'
 rm -rfv css
 echo $'\nCopying:'
 echo '---------'
 cp -rv $1/css .
+
+# components
 echo $'\nDeleting:'
 echo '---------'
 rm -rfv components
 echo $'\nCopying:'
 echo '---------'
 cp -rv $1/components .
+
+# speakers
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv speakers
+echo $'\nCopying:'
+echo '---------'
+cp -rv $1/speakers .
+
+# schedule
+echo $'\nDeleting:'
+echo '---------'
+rm -rfv schedule
+echo $'\nCopying:'
+echo '---------'
+cp -rv $1/schedule .
+
+
+
+
